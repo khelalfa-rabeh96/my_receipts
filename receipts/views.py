c6d3d3783f6e09fd802cdcf0f270a140b416d80f
khelalfa rabah <khelalfarabeh1996@gmail.com>
2023-12-11 14:43:46 +0100

    Write code to pass the test for adding new receipt with total amount only

--- receipts/views.py
+++ receipts/views.py
@@ -1,5 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def receipts_list(request):
-    return render(request, 'receipt_list.html')
+    
+    return render(request, 'receipt_list.html', {
+    'new_total_amount': request.POST.get('new_total_amount', ''),
+    })
