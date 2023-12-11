d7f88ef2800b0aefcdb6ff96c305f4580a336839
khelalfa rabah <khelalfarabeh1996@gmail.com>
2023-12-11 20:15:51 +0100

    Move receipts urls to instead of gobal urls receipts app

--- my_receipts/urls.py
+++ my_receipts/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from receipts.views import receipts_list
 
 urlpatterns = [
-    path('', receipts_list, name="receipts-list"),
+    path('receipts', include('receipts.urls')),
     path('admin/', admin.site.urls),
 ]
